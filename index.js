9e01d347809754babc7530cc8dab7c9fa621a217
Manuelevaristo <findaevaristo@gmail.com>
2021-10-05 16:57:01 -0300

    second commit

--- index.js
+++ index.js
@@ -6,12 +6,36 @@ const port = 3000; // Const para armanezar a porta do servidor
 app.set("view engine", "ejs");
 // Substituição de function por arrow function
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+const pokedex=[];
+let message = "";
 app.get("/", (req, res) => {
-  res.send("Hello World");
+
+  setTimeout(() => {
+    message = "";
+  }, 1000);
+  res.render("index", {
+    pokedex,
+    message
+  });
+});
+app.get("/cadastro", (req, res) =>{
+  res.render("cadastro");
+});
+app.post("/new", (req, res) => {
+  const pokemon={ numero, nome, tipo, imagem, descricao, altura, peso, categoria, habilidade} = req.body;
+  pokedex.push(pokemon);
+  message = `Parabéns o pokemon ${nome}, foi cadastrado com sucesso!`;
+  res.redirect("/");
+});
+app.get("/detalhes/:id",(req, res) =>{
+  const id=req.params.id;
+  const pokemon=pokedex[id];
+  res.render("detalhes",{
+    pokemon,
+  })
 });
 
-app.get("/index", (req, res) => {
-    res.render("index", { titulo: "Blue" });
-  });
+
 // Adicionando a const port e uma arow function de callback para mostrar no console que o servidor está rodando.
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
